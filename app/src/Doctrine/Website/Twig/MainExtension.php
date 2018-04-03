b2635cc38c2fcebbcd89e9075349232c2f726592
Jonathan H. Wage <jwage@opensky.com>
2018-04-03 04:10:29 +0100

    Add initial team page.

--- app/src/Doctrine/Website/Twig/MainExtension.php
+++ app/src/Doctrine/Website/Twig/MainExtension.php
@@ -10,16 +10,30 @@ use Twig_SimpleTest;
 
 class MainExtension extends Twig_Extension
 {
+    /** @var array*/
+    private $team;
+
+    public function __construct(array $team)
+    {
+        $this->team = $team;
+    }
+
     public function getFunctions()
     {
         return [
             new Twig_SimpleFunction('get_asset_url', array($this, 'getAssetUrl')),
+            new Twig_SimpleFunction('get_team', array($this, 'getTeam')),
         ];
     }
 
     public function getAssetUrl(string $path, string $siteUrl)
     {
         return $siteUrl.$path.'?'.$this->getAssetCacheBuster($path);
+    }
+
+    public function getTeam() : array
+    {
+        return $this->team;
     }
 
     private function getAssetCacheBuster(string $path) : string
