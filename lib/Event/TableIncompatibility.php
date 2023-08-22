91e526a9b7f96cb93030ebfa2442861ae82fc62c
Claudio Zizza <claudio@budgegeria.de>
2023-08-23 00:01:41 +0200

    Create eventlistener to handle dbal rst table error
    This is an unfavorable workaround to progress the update to php 8.2

---
+++ lib/Event/TableIncompatibility.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Website\Event;
+
+use Doctrine\RST\Event\PreParseDocumentEvent;
+
+use function str_contains;
+use function str_replace;
+
+final class TableIncompatibility
+{
+    private const BEFORE = '| **SQL Server**           |         +----------------------------------------------------------+';
+    private const AFTER  = '| **SQL Server**           |         |                                                          |';
+
+    public function preParseDocument(PreParseDocumentEvent $event): void
+    {
+        if (! str_contains($event->getContents(), self::BEFORE)) {
+            return;
+        }
+
+        $content = str_replace(self::BEFORE, self::AFTER, $event->getContents());
+        $event->setContents($content);
+    }
+}
