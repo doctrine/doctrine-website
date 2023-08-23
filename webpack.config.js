f2e649056589f0280e016fd5eea3160179b34862
Grégoire Paris <postmaster@greg0ire.fr>
2023-08-23 13:55:30 +0200

    Switch to asset modules
    file-loader is deprecated since webpack 4 in favor of asset modules,
    which allow to load assets without installing or configuring
    file-loader.
    
    See https://v4.webpack.js.org/loaders/file-loader/
    See https://webpack.js.org/guides/asset-modules/
    
    Closes #502

--- webpack.config.js
+++ webpack.config.js
@@ -81,11 +81,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf|.svg)$/,
-                loader: 'file-loader',
-                options: {
-                    name: 'assets/[name].[ext]',
-                    publicPath: '/frontend/'
-                }
+                type: 'asset/resource'
             }
         ]
     },
