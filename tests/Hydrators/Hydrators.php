dc307b861628e3ebdef0ea243239c1ff87a7db09
Claudio Zizza <claudio@budgegeria.de>
2023-09-18 22:43:48 +0200

    Increase code coverage for Hydrators namespace

--- tests/Hydrators/Hydrators.php
+++ tests/Hydrators/Hydrators.php
@@ -10,7 +10,11 @@ use Doctrine\SkeletonMapper\Mapping\ClassMetadataInstantiator;
 use Doctrine\SkeletonMapper\ObjectIdentityMap;
 use Doctrine\SkeletonMapper\ObjectManager;
 use Doctrine\SkeletonMapper\ObjectRepository\ObjectRepositoryFactory;
+use Doctrine\SkeletonMapper\ObjectRepository\ObjectRepositoryInterface;
 use Doctrine\SkeletonMapper\Persister\ObjectPersisterFactory;
+use Doctrine\Website\Model\Contributor;
+use Doctrine\Website\Repositories\ContributorRepository;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use ReflectionObject;
 
@@ -25,10 +29,18 @@ abstract class Hydrators extends TestCase
      */
     protected function createHydrator(string $className)
     {
+        $objectRepositoryFactory = new ObjectRepositoryFactory();
+
+        /** @var ObjectRepositoryInterface<object>&MockObject $contributorRepository */
+        $contributorRepository = $this->createMock(ContributorRepository::class);
+        $contributorRepository->method('findOneByGithub')->willReturn(new Contributor());
+
+        $objectRepositoryFactory->addObjectRepository(Contributor::class, $contributorRepository);
+
         $objectManager = new ObjectManager(
-            new ObjectRepositoryFactory(),
+            $objectRepositoryFactory,
             new ObjectPersisterFactory(),
-            new ObjectIdentityMap(new ObjectRepositoryFactory()),
+            new ObjectIdentityMap($objectRepositoryFactory),
             new ClassMetadataFactory(new ClassMetadataInstantiator()),
             new EventManager(),
         );
