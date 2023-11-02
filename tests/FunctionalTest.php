de4dcf270ba1417c7fa366225f6d3b49f9508b46
Claudio Zizza <859964+SenseException@users.noreply.github.com>
2023-11-02 21:19:55 +0100

    Improve website build time in testing workflows (#551)
    * Move website data files to config directory
    
    * Reduce projects for builds of test env
    
    Reducing amount of projects for tests will improve build time of the website
    
    * Adapt functional tests to use existing projects
    
    The test environment has only certain projects available for performance reasons
    
    * Use test environment for website build in CI
    
    * Use default cache directory in test environment

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -45,21 +45,21 @@ class FunctionalTest extends TestCase
         $projectRepository = $container->get(ProjectRepository::class);
         self::assertInstanceOf(ProjectRepository::class, $projectRepository);
 
-        $project = $projectRepository->findOneBySlug('orm');
+        $project = $projectRepository->findOneBySlug('inflector');
 
         $versions = $project->getVersions();
 
         $firstVersion = end($versions);
         self::assertInstanceOf(ProjectVersion::class, $firstVersion);
 
-        self::assertSame('2.0', $firstVersion->getName());
+        self::assertSame('1.0', $firstVersion->getName());
 
         $tags = $firstVersion->getTags();
 
-        self::assertCount(3, $tags);
+        self::assertCount(2, $tags);
 
-        self::assertSame('2.0.2', $firstVersion->getLatestTag()->getName());
-        self::assertSame('2.0.0', $firstVersion->getFirstTag()->getName());
+        self::assertSame('v1.0.1', $firstVersion->getLatestTag()->getName());
+        self::assertSame('v1.0', $firstVersion->getFirstTag()->getName());
     }
 
     public function testHomepageEditLink(): void
@@ -198,23 +198,20 @@ class FunctionalTest extends TestCase
 
     public function testLinks(): void
     {
-        $crawler = $this->assertValid('/projects/doctrine-orm/en/2.6/reference/events.html');
-        self::assertStringContainsString('<a href="events.html#reference-events-lifecycle-events">lifecycle events</a>', $crawler->html());
+        $crawler = $this->assertValid('/projects/doctrine-annotations/en/1.6/annotations.html');
+        self::assertStringContainsString('<a href="https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md">PSR-0 specification</a>', $crawler->html());
 
-        $crawler = $this->assertValid('/projects/doctrine-dbal/en/2.8/reference/data-retrieval-and-manipulation.html');
-        self::assertStringContainsString('<a href="types.html#mappingMatrix">Types</a>', $crawler->html());
+        $crawler = $this->assertValid('/projects/doctrine-annotations/en/1.10/annotations.html');
+        self::assertStringContainsString('<a href="https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md">PSR-0 specification</a>', $crawler->html());
 
-        $crawler = $this->assertValid('/projects/doctrine-orm/en/2.6/reference/architecture.html');
-        self::assertStringContainsString('<a href="../cookbook/implementing-wakeup-or-clone.html">do so safely</a>', $crawler->html());
+        $crawler = $this->assertValid('/projects/doctrine-inflector/en/1.3/index.html');
+        self::assertStringContainsString('<a href="index.html#classify">Classify</a>', $crawler->html());
 
-        $crawler = $this->assertValid('/projects/doctrine-orm/en/2.6/reference/annotations-reference.html');
-        self::assertStringContainsString('<a href="annotations-reference.html#annref_column_result">@ColumnResult</a>', $crawler->html());
+        $crawler = $this->assertValid('/projects/doctrine-inflector/en/2.0/index.html');
+        self::assertStringContainsString('<a href="index.html#classify">Classify</a>', $crawler->html());
 
-        $crawler = $this->assertValid('/projects/doctrine-dbal/en/2.8/reference/events.html');
-        self::assertStringContainsString('<a href="platforms.html">Platforms</a>', $crawler->html());
-
-        $crawler = $this->assertValid('/projects/doctrine-orm/en/2.6/reference/improving-performance.html');
-        self::assertStringContainsString('<a href="../tutorials/extra-lazy-associations.html">tutorial</a>', $crawler->html());
+        $crawler = $this->assertValid('/projects/doctrine-inflector/en/2.0/index.html');
+        self::assertStringContainsString('<a href="https://sourcemaking.com/design_patterns/null_object">Null Object design pattern</a>', $crawler->html());
     }
 
     public function testSitemap(): void
@@ -259,13 +256,13 @@ class FunctionalTest extends TestCase
 
         self::assertStringContainsString("placeholder: 'Search'", $crawler->html());
 
-        $crawler = $this->assertValid('/projects/migrations.html');
+        $crawler = $this->assertValid('/projects/annotations.html');
 
-        self::assertStringContainsString("placeholder: 'Search Migrations'", $crawler->html());
+        self::assertStringContainsString("placeholder: 'Search Annotations'", $crawler->html());
 
-        $crawler = $this->assertValid('/projects/doctrine-migrations/en/1.7/index.html');
+        $crawler = $this->assertValid('/projects/doctrine-annotations/en/1.6/index.html');
 
-        self::assertStringContainsString("placeholder: 'Search Migrations 1.7'", $crawler->html());
+        self::assertStringContainsString("placeholder: 'Search Annotations 1.6'", $crawler->html());
     }
 
     public function testContribute(): void
