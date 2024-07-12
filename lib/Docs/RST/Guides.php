70134aaca5dfd378bbf6f77b37f2602fa54950e8
Jaapio <jaap@ijaap.nl>
2024-07-12 15:10:35 +0200

    Introduce phpdoc guides builder
    I introduced an interface to be able to switch between the different
    implementations. The first setup of guides is done at the required locations and allows us to render the documentation. However this doesn't work yet without issues. More might be needed consider this work in progress.

---
+++ lib/Docs/RST/Guides.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Website\Docs\RST;
+
+use Flyfinder\Finder;
+use Flyfinder\Specification\Glob;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use League\Tactician\CommandBus;
+use phpDocumentor\Guides\Compiler\CompilerContext;
+use phpDocumentor\Guides\Handlers\CompileDocumentsCommand;
+use phpDocumentor\Guides\Handlers\ParseDirectoryCommand;
+use phpDocumentor\Guides\Handlers\RenderCommand;
+use phpDocumentor\Guides\Nodes\ProjectNode;
+
+final class Guides implements BuilderInterface
+{
+
+    public function __construct(
+        private readonly CommandBus $commandBus
+    ) {
+    }
+
+
+    #[\Override]
+    public function build(string $directory, string $targetDirectory = 'output'): void
+    {
+        $sourceFileSystem = new Filesystem(new Local($directory));
+        $sourceFileSystem->addPlugin(new Finder());
+
+        $projectNode = new ProjectNode();
+        $documents = $this->commandBus->handle(
+            new ParseDirectoryCommand(
+                $sourceFileSystem,
+                '',
+                'rst',
+                $projectNode,
+                new Glob('/**/sidebar.rst')
+            ),
+        );
+
+        $documents = $this->commandBus->handle(
+            new CompileDocumentsCommand($documents, new CompilerContext($projectNode))
+        );
+
+        $destinationFileSystem = new Filesystem(new Local($targetDirectory));
+
+        $this->commandBus->handle(
+            new RenderCommand(
+                'html',
+                $documents,
+                $sourceFileSystem,
+                $destinationFileSystem,
+                $projectNode,
+            ),
+        );
+    }
+
+    #[\Override]
+    public function getDocuments(): array
+    {
+        return [];
+    }
+}
