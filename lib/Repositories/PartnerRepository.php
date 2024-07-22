820609648e7d1d87c338a724446f15a03bb3e865
Claudio Zizza <claudio@budgegeria.de>
2024-07-22 22:20:10 +0200

    Create orm structure for partners

--- lib/Repositories/PartnerRepository.php
+++ lib/Repositories/PartnerRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Doctrine\Website\Repositories;
 
-use Doctrine\SkeletonMapper\ObjectRepository\BasicObjectRepository;
+use Doctrine\ORM\EntityRepository;
 use Doctrine\Website\Model\Partner;
 use InvalidArgumentException;
 
@@ -12,22 +12,13 @@ use function sprintf;
 
 /**
  * @template T of Partner
- * @template-extends BasicObjectRepository<T>
+ * @template-extends EntityRepository<T>
  */
-class PartnerRepository extends BasicObjectRepository
+class PartnerRepository extends EntityRepository
 {
-    /** @return Partner[] */
-    public function findAll(): array
-    {
-        /** @var Partner[] $partners */
-        $partners = parent::findAll();
-
-        return $partners;
-    }
-
     public function findOneBySlug(string $slug): Partner
     {
-        $partner = $this->findOneBy(['slug' => $slug]);
+        $partner = $this->find($slug);
 
         if ($partner === null) {
             throw new InvalidArgumentException(sprintf('Could not find Partner with slug "%s"', $slug));
