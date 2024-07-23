498d4e9c3094fa83f4068f8661c1ada15c24cf3f
Claudio Zizza <claudio@budgegeria.de>
2024-07-23 22:48:53 +0200

    Remove last remnants from SkeletonMapper

--- lib/DataSources/DataSource.php
+++ lib/DataSources/DataSource.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Doctrine\Website\DataSources;
 
-use Doctrine\SkeletonMapper\DataSource\DataSource as SkeletonMapperDataSource;
-
-interface DataSource extends SkeletonMapperDataSource
+interface DataSource
 {
     /** @return mixed[][] */
     public function getSourceRows(): array;
