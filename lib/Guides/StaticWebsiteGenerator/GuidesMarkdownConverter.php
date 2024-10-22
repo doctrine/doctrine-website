1eee5aa28feec4d837bfb2c4f4ca94a6ea511746
Jaapio <jaap@ijaap.nl>
2024-10-22 22:58:10 +0200

    Use guides to parse and render markdown

---
+++ lib/Guides/StaticWebsiteGenerator/GuidesMarkdownConverter.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doctrine\Website\Guides\StaticWebsiteGenerator;
+
+use Doctrine\StaticWebsiteGenerator\SourceFile\SourceFile;
+use Doctrine\StaticWebsiteGenerator\SourceFile\SourceFileConverter;
+use League\Flysystem\Adapter\NullAdapter;
+use League\Flysystem\Filesystem;
+use Override;
+use phpDocumentor\Guides\NodeRenderers\NodeRenderer;
+use phpDocumentor\Guides\Nodes\DocumentNode;
+use phpDocumentor\Guides\Nodes\ProjectNode;
+use phpDocumentor\Guides\Parser;
+use phpDocumentor\Guides\RenderContext;
+
+final readonly class GuidesMarkdownConverter implements SourceFileConverter
+{
+    /** @param NodeRenderer<DocumentNode> $nodeRenderer */
+    public function __construct(
+        private Parser $parser,
+        private NodeRenderer $nodeRenderer,
+    ) {
+    }
+
+    /** @inheritDoc */
+    #[Override]
+    public function getExtensions(): array
+    {
+        return ['md', 'markdown'];
+    }
+
+    #[Override]
+    public function convertSourceFile(SourceFile $sourceFile): string
+    {
+        $document = $this->parser->parse($sourceFile->getContents(), 'md');
+
+        $renderContext = RenderContext::forDocument(
+            $document,
+            [$document],
+            new Filesystem(new NullAdapter()),
+            new Filesystem(new NullAdapter()),
+            '/',
+            'html',
+            new ProjectNode(),
+        );
+
+        return $this->nodeRenderer->render($document, $renderContext);
+    }
+}
